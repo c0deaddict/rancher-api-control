3862b438fcea0aef4f2c66f361e5856cdfb45eb2
Edward Marshall <ruckafella@gmail.com>
2016-02-24 20:20:30 +0000

    Moved script location

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 echo "Sleeping 60 seconds to wait for InfluxDB to be deployed"
 sleep 60
 echo "Running confd to obtain InfluxDB host IP address for the graphite.host value"
-/bin/confd -onetime -backend rancher -prefix /latest
+/usr/bin/confd -onetime -backend rancher -prefix /latest
 
 /usr/local/bin/update.sh
 echo "API Updated. Process completed, exiting"
