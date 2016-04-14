0299c5017bdf6f76dbcc1bbfce7593248fc29af4
Jos van Bakel <jos@kalooga.com>
2016-04-14 16:51:43 +0200

    Fix env var in tmpl + rename influx to graphite

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "Sleeping 60 seconds to wait for InfluxDB to be deployed"
+echo "Sleeping 60 seconds to wait for Graphite to be deployed"
 sleep 60
 
-echo "Running confd to obtain InfluxDB host IP address for the graphite.host value"
+echo "Running confd to obtain Graphite host IP address for the graphite.host value"
 /usr/bin/confd -onetime -backend rancher -prefix /latest -log-level debug
 
 echo "Updating the API"
